10473f331bede31bfbe99c74ea5b0fd7229985b9
Flyingblu <realflyingblu@gmail.com>
2021-12-07 15:38:49 +0800

    [CertChain] Add description

--- src/main/java/com/flyingblu/certAnalysis/AnalyzeCertChain.java
+++ src/main/java/com/flyingblu/certAnalysis/AnalyzeCertChain.java
@@ -15,6 +15,11 @@ import java.sql.SQLException;
 import java.util.HashMap;
 
 /**
+ * Objective: Validate the certificate.
+ * Method: First check whether certificate chain can be trusted or not with the
+ * Mozilla Firefox's trust anchors. Then for the trusted certificate chains, check
+ * if there are any certificate revoked by the CRL (can analyze the level of revoked
+ * certificates).
  * Note: trusted CA certificates retrieved on 06/12/2021 from Mozilla Firefox
  */
 public class AnalyzeCertChain {
@@ -29,19 +34,22 @@ public class AnalyzeCertChain {
         final KeyStore ks = keyStoreUtil.loadKeyStore(KEYSTORE_PATH, KEYSTORE_PASSWD);
         CertPathValidator cpValidator = CertPathValidator.getInstance("PKIX");
         PKIXParameters params = new PKIXParameters(ks);
+        // The revocation status will be validated manually
         params.setRevocationEnabled(false);
         final var errs = new HashMap<String, Integer>();
 
         for (var certs : new DBCertFetcher(DB_PATH)) {
+            // Check if is trusted
             final var certPath = CertUtil.getCertPathFromArray(certs.certs);
             try {
                 final var result = (PKIXCertPathValidatorResult) cpValidator.validate(certPath, params);
-                errs.put("PASS", errs.getOrDefault("PASS", 0) + 1);
+                errs.put("TRUSTPASS", errs.getOrDefault("TRUSTPASS", 0) + 1);
             } catch (CertPathValidatorException e) {
                 errs.put(e.toString(), errs.getOrDefault(e.toString(), 0) + 1);
             }
         }
 
+        // Output results
         try (final var fw = new FileWriter(CSV_FILE_PATH);
              final var csvPrinter = new CSVPrinter(fw, CSVFormat.Builder.create().setHeader(CSV_HEADER).build())) {
             errs.forEach((err, cnt) -> {
