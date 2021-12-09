7f574b28d4ad6643a0c2c783ebb1879fca874e85
Jinhong Guo <jinhongguo@Jinhongs-MacBook-Pro.local>
2021-12-09 21:45:22 +0800

    modify analyzeCa file

--- src/main/java/com/flyingblu/certAnalysis/certAnalyzer/AnalyzeCA.java
+++ src/main/java/com/flyingblu/certAnalysis/certAnalyzer/AnalyzeCA.java
@@ -12,13 +12,44 @@ public class AnalyzeCA {
     public static void main(String[] args) throws SQLException {
         final String DB_PATH = "cert.sqlite";
         final String FILE_PATH="./AnalyzeCA.csv";
-
+//        int total_certificate = 0;
         HashMap<String, Integer> issuer_count = new HashMap<>();
+        int total = 0;
         for (var certs : new DBCertFetcher(DB_PATH)) {
             for(var cert : certs.certs){
-                issuer_count.put(cert.getIssuerX500Principal().toString(), issuer_count.getOrDefault(cert.getIssuerX500Principal().toString(), 0)+1);
+                total++;
+                String organization = " ";
+                if(cert.getIssuerX500Principal().toString().split("O=").length > 1){
+                    organization = cert.getIssuerX500Principal().toString().split("O=")[1].split(",")[0].replace("\"", "");
+                }else{
+                    organization = cert.getIssuerX500Principal().toString().split("CN=")[1];
+                }
+                if(organization.toLowerCase().contains("daddy")){
+                    organization = "GoDaddy";
+                }
+                if(organization.toLowerCase().contains("digital signature trust")){
+                    organization = "IdenTrust";
+                }
+                if(organization.toLowerCase().contains("globalsign")){
+                    organization = "GlobalSign";
+                }
+                if(organization.toLowerCase().contains("internet security")){
+                    organization = "Let's Encrypt";
+                }
+                if(organization.toLowerCase().contains("baltimore")){
+                    organization = "DigiCert Inc";
+                }
+                if(organization.toLowerCase().contains("comodo")){
+                    organization = "Comodo CA Limited";
+                }
+                if(organization.toLowerCase().contains("unizeto")){
+                    organization = "Unizeto";
+                }
+                issuer_count.put(organization, issuer_count.getOrDefault(organization, 0)+1);
+//                issuer_count.put(cert.getIssuerX500Principal().toString(), issuer_count.getOrDefault(cert.getIssuerX500Principal().toString(), 0)+1);
             }
         }
+        System.out.println(total);
         HashMap<String, Integer> issuer_percent = new HashMap<>();
         for(Map.Entry<String, Integer> entry : issuer_count.entrySet()){
             if(entry.getValue() > 1000){
@@ -32,14 +63,17 @@ public class AnalyzeCA {
 
         try {
             CsvWriter csvWriter=new CsvWriter(FILE_PATH, ',', Charset.forName("GBK"));
-            String[] headers={"issuers","number"};
+            String[] headers={"Rank", "issuer","number", "percentage"};
             csvWriter.writeRecord(headers);
+            int count = 1;
             for(Map.Entry<String, Integer> t : rank){
-                if (Objects.equals(t.getKey(), "others")) {
-                    csvWriter.writeRecord(new String[] {t.getKey(), String.valueOf(t.getValue())});
-                }else{
-                    csvWriter.writeRecord(new String[] {t.getKey().split(",")[0].split("=")[1], String.valueOf(t.getValue())});
-                }
+                csvWriter.writeRecord(new String[] {String.valueOf(count), t.getKey(), String.valueOf(t.getValue()), String.format("%.2f", (double)t.getValue()/total*100) + "%"});
+//                if (Objects.equals(t.getKey(), "others")) {
+//                    csvWriter.writeRecord(new String[] {t.getKey(), String.valueOf(t.getValue())});
+//                }else{
+////                    csvWriter.writeRecord(new String[] {t.getKey().split(",")[0].split("=")[1], String.valueOf(t.getValue())});
+//                }
+                count++;
             }
             csvWriter.close();
         } catch (IOException e) {
